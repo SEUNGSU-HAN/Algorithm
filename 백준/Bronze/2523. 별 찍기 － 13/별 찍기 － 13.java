0213a2369d74a64afc896c2e7884fbe7c2f9b9f2
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-03 13:50:22 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+
+	public static void main(String[] args) throws Exception{
+		//입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		//로직
+		for (int i = 0; i < 2*N-1; i++) {
+			//별
+			for (int j = 0; j < -Math.abs(i-(N-1))+N; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
