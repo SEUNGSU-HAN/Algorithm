18ef5251b35b4cc3b94e5da275026a16c3c979e5
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-12 14:29:19 +0900

    [Gold III] Title: 게리맨더링, Time: 108 ms, Memory: 13712 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,108 +1,127 @@
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main {
-	static int N, min, cnt;
+public class Main {
+	static int N;
 	static int[] people;
-	static int[][] map;
 	static boolean[] visited;
-	static boolean[] teamVisit;
-	static ArrayList<Integer>[] teams;
-
-	public static void main(String[] args) throws IOException {
+	static ArrayList<Integer>[] graph;
+	static boolean[] tfVisited;
+	static int minDiff = Integer.MAX_VALUE;
+	static int sum;
+	
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
+		
+		/* 초기화 */
+		people = new int[N+1];
+		visited = new boolean[N+1];
+		tfVisited = new boolean[N+1];
+		graph = new ArrayList[N+1];
+		for (int i = 0; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
 		st = new StringTokenizer(br.readLine());
-		people = new int[N];
-		map = new int[N][N];
-
-		visited = new boolean[N];
-		cnt = 0;
-
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			people[i] = Integer.parseInt(st.nextToken());
 		}
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < n; j++) {
-				int a = Integer.parseInt(st.nextToken());
-				map[i][a - 1] = 1;
+			int temp = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < temp; j++) {
+				int n = Integer.parseInt(st.nextToken());
+				graph[i].add(n);
+				graph[n].add(i);
 			}
 		}
-		min = Integer.MAX_VALUE;
-		subset(0);
-		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+		for (int i = 1; i <= N; i++) {
+			sum += people[i];
+		}
+		
+		/* 로직 */
+		subSet(1, 0);
+		
+		/* 출력 */
+		if(minDiff == Integer.MAX_VALUE) minDiff = -1;
+		System.out.println(minDiff);
 	}
 
-	private static void subset(int depth) {
-		if (depth == N) {
-			cnt = getAns(); // 팀 => subset으로 팀 나눠짐
-			if (cnt == -1) return;
-			min = Math.min(cnt, min);
+	private static void subSet(int cnt, int tot) {
+		if(cnt == N+1) {
+			ArrayList<Integer> tp = new ArrayList<>(); //true 구역
+			ArrayList<Integer> fp = new ArrayList<>();//false 구역
+			for (int i = 1; i <= N; i++) {
+				if(visited[i]) tp.add(i);
+				else fp.add(i);
+			}
+			
+			//불가능한 방법1 - 한쪽 선거구가 비었음
+			if(tp.size() == 0 || fp.size() == 0) return;
+			int con = bfs(tp, fp);
+			minDiff = Math.min(minDiff, con);
 			return;
 		}
-		visited[depth] = true;
-		subset(depth + 1);
-		visited[depth] = false;
-		subset(depth + 1);
+		visited[cnt] = true;
+		subSet(cnt+1, tot+people[cnt]);
+		visited[cnt] = false;
+		subSet(cnt+1, tot);
 	}
 
-	private static int getAns() {
-		List<Integer> teamA = new ArrayList<>();
-		List<Integer> teamB = new ArrayList<>();
+	private static int bfs(ArrayList<Integer> tp, ArrayList<Integer> fp) {
+		Queue<Integer> q = new LinkedList<>();
 
-		for (int i = 0; i < visited.length; i++) {
-			if (visited[i])
-				teamA.add(i);
-			else
-				teamB.add(i);
-		}
-		if (teamA.size() == 0 || teamB.size() == 0) return -1;
-		
-		teamVisit = new boolean[N];
-
-		bfs(teamA);
-		bfs(teamB);
-
-		for (int i = 0; i < teamVisit.length; i++) {
-			if (!teamVisit[i])
-				return -1;
-		}
-
-		int sumA = 0, sumB = 0;
-		for (int a : teamA) {
-			sumA += people[a];
-		}
-		for (int b : teamB) {
-			sumB += people[b];
-		}
-		return Math.abs(sumB - sumA);
-	}
-
-	private static void bfs(List<Integer> team) {
-		Queue<Integer> que = new ArrayDeque<>();
-		que.offer(team.get(0));
-		teamVisit[team.get(0)] = true;
-		while (!que.isEmpty()) {
-			int a = que.poll();
-			for (int i = 0; i < team.size(); i++) {
-				if (teamVisit[team.get(i)]) continue;
-				if (map[a][team.get(i)] == 1) {
-					que.offer(team.get(i));
-					teamVisit[team.get(i)] = true;
+		int temp = tp.get(0);
+		int tConn = people[temp];
+		q.offer(temp);
+		tfVisited[temp] = true;
+		//true 구역 집계
+		while(!q.isEmpty()) {
+			temp = q.poll();
+			for (int i = 0; i < graph[temp].size(); i++) {
+				int next = graph[temp].get(i);
+				if(check(tp, next)) {
+					q.offer(next);
+					tConn += people[next];
+					tfVisited[next] = true;
 				}
 			}
 		}
+		
+		//false 구역 집계
+		temp = fp.get(0);
+		int fConn = people[temp];
+		q.offer(temp);
+		tfVisited[temp] = true;
+		while(!q.isEmpty()) {
+			temp = q.poll();
+			for (int i = 0; i < graph[temp].size(); i++) {
+				int next = graph[temp].get(i);
+				if(check(fp, next)) {
+					q.offer(next);
+					fConn += people[next];
+					tfVisited[next] = true;
+				}
+			}
+		}
+		tfVisited = new boolean[N+1];
+		if(sum != (tConn+fConn)) return Integer.MAX_VALUE;
+		
+		return Math.abs(tConn-fConn);
 	}
+
+	private static boolean check(ArrayList<Integer> p, int next) {
+		if(p.contains(next) && !tfVisited[next]) return true;
+		return false;
+	}
+
+
+
 }
