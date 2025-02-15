e827e9bd7804620c4ff5f1b7bcac01f27d992813
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-16 00:05:44 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	static int A, B;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt(); B = sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+
+}
