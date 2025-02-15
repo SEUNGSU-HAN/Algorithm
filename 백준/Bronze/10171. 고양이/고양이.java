0af6029d674172dd8fc7296ce1f69ae8e51549ce
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-15 23:31:58 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\").append("    ").append("/").append("\\").append("\n");
+		sb.append(" ").append(")").append("  ").append("(").append(" ").append("\'").append(")").append("\n");
+		sb.append("(").append("  ").append("/").append("  ").append(")").append("\n");
+		sb.append(" \\").append("(__)").append("|");
+		
+		System.out.print(sb);
+			
+	}
+
+}
