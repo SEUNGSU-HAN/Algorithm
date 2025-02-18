fc16112c686481872b70c187f56ff1022b2c1c03
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-18 15:43:48 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,54 +1,38 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//순서가 의미가 없다. -> 조합 사용 -> nCr
-
-public class Main {
-	static int N;
-	static int M;
-	static int R;
-	static int[] cards;
-	static int[] choice;
-	static int diff;
-	static int sum;
-
-	public static void main(String[] args) throws Exception{
+class Main {
+	static int N,M;
+	static int[] nums;
+	static int max;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		R = 3;
-		diff = Integer.MAX_VALUE;
-		sum = 0;
-		choice = new int[R];
-		cards = new int[N];
-
+		nums = new int[N];
+		max = 0;
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			cards[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		combi(0, 0, 0);
-		
-		System.out.println(sum);
+		getAns(0,0,0);
+		System.out.println(max);
 	}
-	
-	public static void combi(int cnt, int start, int tot) {
-		if(cnt == R) {
-			if(tot > M) return;
-			int d = Math.abs(M-tot);
-			if(d <= diff) {
-				diff = d;
-				sum = tot;
-			}
+	private static void getAns(int depth,int start,int sum) {
+		if(sum>M) return;
+		if(depth==3) {
+			max = Math.max(max,sum);
 			return;
 		}
 		for (int i = start; i < N; i++) {
-			choice[cnt] = cards[i];
-			combi(cnt+1, i+1, tot+cards[i]);
-			choice[cnt] = 0;
+			getAns(depth+1,i+1,sum+nums[i]);
 		}
 	}
-
 }
