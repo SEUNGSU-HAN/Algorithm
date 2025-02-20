a9cea3c4cea72c7b612fe1f1b1a516d298c7ec81
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-20 10:21:37 +0900

    [Gold V] Title: 보물섬, Time: 484 ms, Memory: 222944 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+//보물섬
+public class Main {
+	static int N, M;
+	static int[][] board;
+	static int[][] tmpBoard;
+	static boolean[][] visited;
+	static ArrayList<int[]> land;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	static int minDis;
+
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		/* 초기화 */
+		board = new int[N][M];
+		tmpBoard = new int[N][M];
+		land = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			char[] cl = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				if(cl[j] == 'L') {
+					board[i][j] = 0;
+					land.add(new int[] {i, j});
+				}
+				else board[i][j] = -1;
+			}
+		}
+		
+		/* 로직 */
+		for (int i = 0; i < land.size(); i++) {
+			copyBoard();
+			visited = new boolean[N][M];
+			bfs(i, 0);
+		}
+		
+		/* 출력 */
+		System.out.print(minDis);
+	}
+
+	private static void copyBoard() {
+		for (int i = 0; i < N; i++) {
+			System.arraycopy(board[i], 0, tmpBoard[i], 0, board[0].length);
+		}
+	}
+
+	private static void bfs(int start, int cnt) {
+		Queue<int[]> q = new ArrayDeque<>();
+		int[] sp = {land.get(start)[0], land.get(start)[1], 0};
+		q.offer(sp);
+		visited[sp[0]][sp[1]] = true;
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			tmpBoard[cur[0]][cur[1]] = cur[2];
+			minDis = Math.max(minDis, cur[2]);
+			for (int i = 0; i < 4; i++) {
+				int nr = cur[0]+dr[i];
+				int nc = cur[1]+dc[i];
+				if(check(nr, nc)) {
+					q.offer(new int[] {nr, nc, cur[2]+1});
+					visited[nr][nc] = true;
+				}
+			}
+		}
+		
+	}
+
+	private static boolean check(int nr, int nc) {
+		return ((0 <= nr && nr < N) && (0 <= nc && nc < M) 
+				&& !visited[nr][nc]
+				&& tmpBoard[nr][nc] != -1);
+	}
+
+}
