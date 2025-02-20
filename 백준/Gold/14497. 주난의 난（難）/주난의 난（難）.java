38d6c17b113149c8eee1e4b22af9d54236e349fd
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-20 21:33:58 +0900

    [Gold IV] Title: 주난의 난(難), Time: 184 ms, Memory: 21140 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,19 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
 	static int[] j, c; //주난, 초코바 위치
 	static char[][] board;
-	static char[][] tmpBoard;
 	static boolean[][] visited;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
-	static boolean isCatch;
-	static int jump;
 
 	public static void main(String[] args) throws Exception{
 		/* 입력 */
@@ -27,44 +23,35 @@ public class Main {
 		
 		/* 초기화 */
 		board = new char[N][M];
-		tmpBoard = new char[N][M];
 		for (int i = 0; i < N; i++) {
 			char[] cl = br.readLine().toCharArray();
 			for (int j = 0; j < M; j++) {
 				board[i][j] = cl[j];
-				tmpBoard[i][j] = cl[j];
 			}
 		}
+		visited = new boolean[N][M];
 		
-		/* 로직 */
-		while(!isCatch) {
-			visited = new boolean[N][M];
-			jump++;
-			bfs();
-		}
-		
-		/* 출력 */
-		System.out.print(jump);
+		/* 로직+출력 */
+		bfs();
 	}
-
+	//수정후(개선 버전)
 	private static void bfs() {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(j);
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {j[0], j[1], 0});
 		visited[j[0]][j[1]] = true;
-		loop:
 		while(!q.isEmpty()) {
 			int[] cur = q.poll();
+			if(cur[0] == c[0] && cur[1] == c[1]) {
+				System.out.print(cur[2]);
+				return;
+			}
 			for (int i = 0; i < 4; i++) {
 				int nr = cur[0]+dr[i];
 				int nc = cur[1]+dc[i];
 				if(check(nr, nc)) {
-					if(board[nr][nc] == '1') board[nr][nc] = '0';
-					else if(board[nr][nc] == '0') q.offer(new int[] {nr, nc});
-					else {
-						isCatch = true;
-						break loop;
-					}
 					visited[nr][nc] = true;
+					if(board[nr][nc] == '0') q.addFirst(new int[] {nr, nc, cur[2]});
+					else q.offer(new int[] {nr, nc, cur[2]+1});
 				}
 			}
 		}
