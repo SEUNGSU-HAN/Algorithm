deb758315a24a568ffaed914a84234cf9e4fcde2
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-22 01:39:06 +0900

    [Bronze III] Title: 직각삼각형, Time: 108 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int a, b, c;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			b = (int)Math.pow(Integer.parseInt(st.nextToken()), 2);
+			a = (int)Math.pow(Integer.parseInt(st.nextToken()), 2);
+			c = (int)Math.pow(Integer.parseInt(st.nextToken()), 2);
+			if(a == 0 && b == 0 && c == 0) break;
+			if((a == b+c) || (b == a+c) || (c == a+b)) sb.append("right");
+			else sb.append("wrong");
+			sb.append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+}
