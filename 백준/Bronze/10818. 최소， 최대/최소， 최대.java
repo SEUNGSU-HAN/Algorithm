03662c074c2347a3a55d627ba130dc6ae4e7895c
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-02-28 01:53:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 952 ms, Memory: 130204 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int N;
+    static int[] nums;
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        nums = new int[N];
+        for(int i = 0; i < N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        
+        System.out.print(nums[0] + " " + nums[N-1]);
+    }
+}
