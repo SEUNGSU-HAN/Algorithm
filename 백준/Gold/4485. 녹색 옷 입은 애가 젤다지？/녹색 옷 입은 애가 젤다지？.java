6e0c0e80449301b380401ae20eaf448fe645bbd2
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-10 17:33:54 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 156 ms, Memory: 17320 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -5,9 +5,24 @@ public class Main {
 	static final int INF = 999;
 	static int N, test_case = 1;
 	static int[][] board;
+	static int[][] dijk;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
 	static int[] dp;
+	static class Node implements Comparable<Node>{
+		int r, c, w;
+
+		public Node(int r, int c, int w) {
+			this.r = r;
+			this.c = c;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.w, o.w);
+		}
+	}
 
 	public static void main(String[] args) throws Exception{
 		/* 입력 */
@@ -27,29 +42,58 @@ public class Main {
 			}
 			dp = new int[N*N];
 			Arrays.fill(dp, INF);
+			dijk = new int[N][N];
+			for(int i = 0; i < N; i++) {
+				Arrays.fill(dijk[i], INF);
+			}
 			
 			/* 로직 */
 			//1. 다익스트라
-			PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
-			int[] start = {0, 0, board[0][0]};
+//			PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+//			int[] start = {0, 0, board[0][0]};
+//			q.offer(start);
+//			dp[0] = board[0][0];
+//			while(!q.isEmpty()) {
+//				int[] cur = q.poll();
+//				for (int i = 0; i < 4; i++) {
+//					int nr = cur[0]+dr[i];
+//					int nc = cur[1]+dc[i];
+//					if(check(nr, nc)) {
+//						int pre = cur[0]*N+cur[1]%N;
+//						int next = nr*N+nc%N;
+//						if(dp[next] > dp[pre]+board[nr][nc]){
+//							dp[next] = dp[pre]+board[nr][nc];
+//							q.offer(new int[] {nr, nc, board[nr][nc]});
+//						}
+//					}
+//				}
+//			}
+			
+			//개선 ver
+			PriorityQueue<Node> q = new PriorityQueue<>();
+			Node start = new Node(0, 0, board[0][0]);
 			q.offer(start);
-			dp[0] = board[0][0];
+			dijk[0][0] = board[0][0];
 			while(!q.isEmpty()) {
-				int[] cur = q.poll();
+				Node cur = q.poll();
+				
+				if(cur.r == N-1 && cur.c == N-1) break;
+				
 				for (int i = 0; i < 4; i++) {
-					int nr = cur[0]+dr[i];
-					int nc = cur[1]+dc[i];
+					int nr = cur.r+dr[i];
+					int nc = cur.c+dc[i];
 					if(check(nr, nc)) {
-						int pre = cur[0]*N+cur[1]%N;
-						int next = nr*N+nc%N;
-						if(dp[next] > dp[pre] + board[nr][nc]) {
-							dp[next] = dp[pre] + board[nr][nc];
-							q.offer(new int[] {nr, nc, board[nr][nc]});
+						if(dijk[nr][nc] > dijk[cur.r][cur.c]+board[nr][nc]){
+							dijk[nr][nc] = dijk[cur.r][cur.c]+board[nr][nc];
+							q.offer(new Node(nr, nc, dijk[nr][nc]));
 						}
 					}
 				}
 			}
-			sb.append("Problem ").append(test_case++).append(": ").append(dp[N*N-1]).append("\n");
+			
+//			sb.append("Problem ").append(test_case++).append(": ").append(dp[N*N-1]).append("\n");
+			sb.append("Problem ").append(test_case++).append(": ").append(dijk[N-1][N-1]).append("\n");
+			
 			N = Integer.parseInt(br.readLine().trim());
 		}
 		/* 출력 */
