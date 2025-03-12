c41c7d91a8ab0cd1f5389b7882a6ee282f7eb515
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-12 17:27:11 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int[] nums, dp, dp2;
+
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine().trim());
+		
+		/* 초기화 */
+		nums = new int[N+1];
+		dp = new int[N+1];
+		for (int i = 1; i <= N; i++) {
+			nums[i] = Integer.parseInt(br.readLine().trim());
+		}
+		dp[1] = nums[1];
+		if(N == 2) dp[2] = nums[1]+nums[2];
+		
+		/* 로직 */
+		if(N == 3) {
+			dp[2] = nums[1]+nums[2];
+			dp[3] = Math.max(nums[1]+nums[3], nums[2]+nums[3]);
+		}
+		if(N > 3) {
+			dp[3] = Math.max(nums[1]+nums[3], nums[2]+nums[3]);
+			dp[2] = nums[1]+nums[2];
+			for (int i = 4; i <= N; i++) {
+				dp[i] = Math.max(dp[i-2], dp[i-3]+nums[i-1])+nums[i];
+			}
+		}
+		
+		/* 출력 */
+		System.out.print(dp[N]);
+	}
+
+}
