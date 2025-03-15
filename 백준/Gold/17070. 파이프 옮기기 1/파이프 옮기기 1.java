51185bbe961e2d14d4b45c6ada1656a8c59f65b0
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-15 16:07:23 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 276 ms, Memory: 13648 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -9,6 +9,7 @@ public class Main {
 			{{0, 1}, {1, 0}, {1, 1}} //대각선으로 놓였을 때
 	};
 	static int[][] board;
+	static long[][][] dp;
 
 	public static void main(String[] args) throws Exception{
 		/* 입력 */
@@ -24,31 +25,32 @@ public class Main {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		dp = new long[3][N][N];
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < N; j++) {
+//				Arrays.fill(dp[i][j], -1);
+//			}
+//		}
 		
 		/* 로직+출력 */
-		System.out.print(bfs(new int[] {0, 1, 0})); //r, c, 파이프 상태		
+		System.out.print(dfs(0, 1, 0)); //r, c, 파이프 상태		
+		
 	}
 
-	static int bfs(int[] start) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(start);
+	static int dfs(int r, int c, int s) {
+		if(r == N-1 && c == N-1) return 1;
+		
 		int count = 0;
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int s = cur[2];
-			if(cur[0] == N-1 && cur[1] == N-1) count++;
-			for (int i = 0; i < d[s].length; i++) {
-				int nr = cur[0]+d[s][i][0];
-				int nc = cur[1]+d[s][i][1];
-				if(nr == cur[0] && nc == cur[1]) continue;
-				if(check(nr, nc) && board[nr][nc] != 1) {
-					if(i == 2) //대각선으로 갈 경우 현재 위치에서 우,하,대 체크
-						if(!check2(cur[0], cur[1])) continue;
-					q.offer(new int[] {nr, nc, i});
-				}
+		for (int i = 0; i < 3; i++) {
+			int nr = r+d[s][i][0];
+			int nc = c+d[s][i][1];
+			if(nr == r && nc == c) continue;
+			if(check(nr, nc) && board[nr][nc] != 1) {
+				if(i == 2) //대각선으로 갈 경우 현재 위치에서 우,하,대 체크
+					if(!check2(r, c)) continue;
+				count += dfs(nr, nc, i);
 			}
 		}
-		
 		return count;
 	}
 
@@ -60,7 +62,7 @@ public class Main {
 		}
 		return true;
 	}
-
+	
 	static boolean check(int nr, int nc) {
 		return (0 <= nr && nr < N) && (0 <= nc && nc < N);
 	}
