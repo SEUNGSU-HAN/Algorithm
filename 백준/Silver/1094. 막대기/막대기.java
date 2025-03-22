4cd8797f6cc4b093a3d3799f76e9fbd47a1a4f22
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-22 18:59:32 +0900

    [Silver V] Title: 막대기, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -8,21 +8,14 @@ public class Main {
 		/* 입력 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		X = Integer.parseInt(br.readLine().trim());
-		
+        
 		/* 로직 + 출력 */
-		System.out.println(findStick(64, X));
+		//비트마스킹 풀이
+		int idx = 0;
+		int count = 0;
+		while((1<<idx) <= X)
+            if((X & 1<<idx++) != 0) count++;
+		System.out.print(count);
 		
 	}
-
-	static int findStick(int s, int x) {
-		if(s == x) return 1;
-		
-		if(x == 0) return 0;
-
-		int half = s/2;
-		if(x >= half)
-			return findStick(half, x-half) + 1;
-		else return findStick(half, x);
-	}
-
 }
