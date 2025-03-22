bd099bfc2edf7f37ba3f0fc9850b5fa6e986f509
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-22 19:00:22 +0900

    [Silver V] Title: 막대기, Time: 68 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -10,12 +10,8 @@ public class Main {
 		X = Integer.parseInt(br.readLine().trim());
         
 		/* 로직 + 출력 */
-		//비트마스킹 풀이
-		int idx = 0;
-		int count = 0;
-		while((1<<idx) <= X)
-            if((X & 1<<idx++) != 0) count++;
-		System.out.print(count);
+		//라이브러리 활용 풀이
+		System.out.print(Integer.bitCount(X));
 		
 	}
 }
