dcc34e62be86b5cbae8d6ac5820618bf1f35dcb4
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-03-25 17:43:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1572 ms, Memory: 177964 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static Set<Integer> numSet;
+
+	public static void main(String[] args) throws Exception{
+		/* 입력 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine().trim());
+		
+		/* 초기화 */
+		numSet = new TreeSet<>();
+		/* 로직 */
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(br.readLine().trim());
+			numSet.add(n);
+		}
+		
+		/* 출력 */
+		StringBuilder sb = new StringBuilder();
+		for (int n : numSet) {
+			sb.append(n).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+}
