09a55772c629d31ebbc06e6c2036c25e5f89a4dc
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-04-04 11:42:58 +0900

    [Silver II] Title: 이동하기, Time: 376 ms, Memory: 84184 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -27,8 +27,7 @@ public class Main {
 		/* 로직 */
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= M; j++) {
-				dp[i][j] = Collections.max(Arrays.asList(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]))
-							+ board[i-1][j-1];
+				dp[i][j] = Math.max(dp[i-1][j], Math.max(dp[i-1][j-1], dp[i][j-1])) + board[i-1][j-1];
 			}
 		}
 		
