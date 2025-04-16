ddc928242eb6f9e00ad7044643748b7cd0c053c7
SeungSu Han <71253519+SeungSu-Han@users.noreply.github.com>
2025-04-16 13:40:34 +0900

    [D4] Title: 오름차순 줄 세우기, Time: 211 ms, Memory: 96,996 KB -BaekjoonHub

---
+++ SWEA/D4/3263. 오름차순 줄 세우기/오름차순 줄 세우기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int T, N;
+	static int[] kids;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			/* 입력 */
+			N = Integer.parseInt(br.readLine().trim());
+			
+			/* 초기화 */
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			kids = new int[N+1];
+			
+			/* 로직 */
+			int ans = 0;
+			for (int i = 0; i < N; i++) {
+				int n = Integer.parseInt(st.nextToken());
+				kids[n] = kids[n-1]+1;
+				if(ans < kids[n]) ans = kids[n];
+			}
+			
+			/* 출력 */
+			sb.append("#").append(test_case).append(" ").append(N-ans).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+}
