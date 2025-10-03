d56d1e9470207324772089b52cbc732bc44ae732
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2025-10-04 00:05:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 120.17 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,16 +1,14 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr) 
 {
     vector<int> answer;
-    answer.reserve(arr.size());
 
-    for (int x : arr) {
-        if (answer.empty() || answer.back() != x) {
-            answer.emplace_back(x);
-        }
-    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
     return answer;
 }
