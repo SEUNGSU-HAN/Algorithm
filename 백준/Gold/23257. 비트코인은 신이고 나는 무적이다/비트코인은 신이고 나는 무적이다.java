033922409803c2eb3734ee24d25b8a1ce7003169
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-03-27 22:41:36 +0900

    [Gold III] Title: 비트코인은 신이고 나는 무적이다, Time: 96 ms, Memory: 12304 KB -BaekjoonHub

---
+++ 백준/Gold/23257. 비트코인은 신이고 나는 무적이다/비트코인은 신이고 나는 무적이다.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int[] A;
+	static boolean[][] dp;
+
+	public static void main(String[] args) throws Exception{
+		/* 셋팅 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		A = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			A[i] = Math.abs(Integer.parseInt(st.nextToken()));
+		}
+		
+		dp = new boolean[M+1][1024];
+		
+		/* 로직 */		
+		dp[0][0] = true;
+		
+		for(int i=1; i<=M; i++) {			
+			for(int j=0; j<1024; j++) {
+				if(dp[i-1][j]) {
+					for(int n : A) {
+						dp[i][j ^ n] = true;
+					}
+				}
+			}
+		}
+		
+		int m = 0;
+		for(int j=0; j<1024; j++) {
+			if(dp[M][j]) m = Math.max(m, j);
+		}
+		
+		/* 출력 */
+		System.out.print(m);
+	}
+
+}
