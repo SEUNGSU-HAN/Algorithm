7eabfa400424b6320ca5e7e5945078fa43a2d626
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-03-31 22:37:16 +0900

    [Gold IV] Title: N-Queen, Time: 2056 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,66 +1,39 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int[][] board;
-	static ArrayList<Integer> rDiagonal;
-	static ArrayList<Integer> cantCol;
-	static int count;
+	static int N, count;
+	static ArrayList<Integer> colList;
 	
-	public static void main(String[] args) {
-		/* 입력 */
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+	public static void main(String[] args) throws Exception{
+		/* 셋팅 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		/* 초기화 */
-		board = new int[N][N];
-		rDiagonal = new ArrayList<>();
-		cantCol = new ArrayList<>();
+		N = Integer.parseInt(br.readLine());
+		colList = new ArrayList<>();
 		
 		/* 로직 */
-		queen(0, 0);
-
+		backtracking(0, 0, 0, 0);
 		
 		/* 출력 */
-		System.out.println(count);
+		System.out.print(count);
 	}
 
-	private static void queen(int r, int q) {
-		//퀸을 모두 놓았을 때
-		if(q == N) {
+	static void backtracking(int cnt, int colCheck, int rDigCheck, int lDigCheck) {
+		if(cnt == N) {
 			count++;
 			return;
 		}
-		
-		for (int c = 0; c < N; c++) {
-			if(isValid(r, c)) {
-				q++; //퀸 배치
-				board[r][c] =1;
-				cantCol.add(c);
-				rDiagonal.add(r+c);
-				queen(r+1, q); //다음 퀸 배치
-				q--;
-				board[r][c] = 0;
-				cantCol.remove(cantCol.size()-1);
-				rDiagonal.remove(rDiagonal.size()-1);
-			}
+		for(int i=0; i<N; i++) {
+			//열 체크
+			if((colCheck & 1<<i) != 0) continue;
+			
+			//대각선 체크
+			if(((rDigCheck & 1<<(cnt+i)) != 0) || 
+				((lDigCheck & 1<<(cnt-i+N)) != 0)) continue;
+			
+			backtracking(cnt+1, (colCheck | 1<<i), (rDigCheck | 1<<(cnt+i)), (lDigCheck | 1<<(cnt-i+N)));
 		}
-		
 	}
 
-	private static boolean isValid(int r, int c) {
-		if(!cantCol.contains(c) && !rDiagonal.contains(r+c)) {
-			for (int i = 0; i < r; i++) {
-				for (int j = 0; j < N; j++) {
-					if(board[i][j] == 1) {
-						if(c == j) return false;
-						if(Math.abs(r-i) == Math.abs(c-j)) return false;
-					}
-				}
-			}
-			return true;
-		}
-		return false;
-	}
 }
