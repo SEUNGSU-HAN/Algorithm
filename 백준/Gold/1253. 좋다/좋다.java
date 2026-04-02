1bd4264887bd84d9bc5ef569709babde45fa9378
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-04-03 03:02:50 +0900

    [Gold IV] Title: 좋다, Time: 124 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 	static int N;
 	static int[] nums;
-	static boolean[] goodNums;
 	
 	public static void main(String[] args) throws Exception{
 		/* 셋팅 */
@@ -12,7 +11,6 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		
 		nums = new int[N];
-		goodNums = new boolean[N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
@@ -23,28 +21,32 @@ public class Main {
 		Arrays.sort(nums);
 		
 		int count = 0;
-		if(N > 2) {
-			for(int i=0; i<N; i++) {
-				int l=0, r=N-1;
+		
+		for(int i=0; i<N; i++) {
+			int l=0, r=N-1;
+			
+			//투포인터 실시
+			while(l < r) { 
+				//포인터의 위치가 현재 index의 위치인지 판단
+				if(l == i) {
+					l++;
+					continue;
+				}
+				if(r == i) {
+					r--;
+					continue;
+				}
 				
-				//투포인터 실시
-				while(l < r) { 
-					//포인터의 위치가 현재 index의 위치인지 판단
-					if(l == i || r == i) {
-						if(l == i) l++;
-						else r--;
-						continue;
-					}
-					
-					//포인터들 위치의 값 합이 타겟 값인지 확인
-					int cur = nums[l] + nums[r];
-					
-					if(cur < nums[i]) l++;
-					else if(cur > nums[i]) r--;
-					else {
-						count++;
-						break;
-					}
+				//포인터들 위치의 값 합이 타겟 값인지 확인
+				int sum = nums[l] + nums[r];
+				
+				if(sum < nums[i])
+					l++;
+				else if(sum > nums[i])
+					r--;
+				else {
+					count++;
+					break;
 				}
 			}
 		}
