2c7103f22cce1a5754a5a80a478571cb9442dff9
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-04-04 20:51:10 +0900

    [Gold II] Title: 문제집, Time: 524 ms, Memory: 50028 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -3,53 +3,52 @@ import java.util.*;
 
 public class Main {
 	static int N, M;
-	static int[] count;
-	static ArrayList<Integer>[] graph;
-	static PriorityQueue<Integer> q;
+	static ArrayList<Integer>[] outDegree;
+	static int[] inDegree;
 
 	public static void main(String[] args) throws Exception{
-		/* 입력 */
+		/* 셋팅 */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		/* 초기화 */
-		count = new int[N+1];
-		graph = new ArrayList[N+1];
-		for (int i = 0; i <= N; i++) {
-			graph[i] = new ArrayList<>();
+		outDegree = new ArrayList[N+1];
+		for(int i=0; i<=N; i++) {
+			outDegree[i] = new ArrayList<>();
 		}
-		for (int i = 0; i < M; i++) {
+		inDegree = new int[N+1];
+				
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
-			graph[s].add(e);
-			count[e]++;
+			
+			outDegree[s].add(e);
+			inDegree[e]++;
 		}
-		//집입차수0인 애들 먼저 넣되 난이도 낮은 애들먼저 처리
-		// => 처리 가능한 애들중 난이도가 쉬운 애들부터 처리
-		//우선순위 큐 사용하여 숫자 낮은 애들 먼저 ㄱㄱ
-		q = new PriorityQueue<>();
-		
+
 		/* 로직 */
-		//진입차수 0인 애들 큐에 추가
-		for (int i = 1; i <= N; i++) {
-			if(count[i] == 0) q.offer(i);
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1, o2));
+		
+		for(int i=1; i<=N; i++) {
+			if(inDegree[i] == 0) pq.offer(i);
 		}
 		
-		//위상 정렬 스타트
 		StringBuilder sb = new StringBuilder();
-		while(!q.isEmpty()) {
-			int cur = q.poll();
+		while(!pq.isEmpty()) {
+			int cur = pq.poll();
+			
 			sb.append(cur).append(" ");
-			for (int next : graph[cur]) {
-				if(--count[next] == 0) q.offer(next);
+			
+			for(int next : outDegree[cur]) {
+				if(--inDegree[next] == 0) pq.offer(next);
 			}
 		}
 		
 		/* 출력 */
-		System.out.print(sb);
+		System.out.print(sb.toString());
 	}
 
 }
