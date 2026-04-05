58e549f2a07d8857cffbf4e7d3e3dd76957a940e
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-04-06 00:36:14 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 528 ms, Memory: 115004 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -3,20 +3,11 @@ import java.util.*;
 
 public class Main {
 	static final int INF = 100_000_000;
-	static int N, M, result = INF;
-	static int[] dc = {-1, 0, 1};
+	static int N, M;
 	static int[][] board;
 	static int[][][] dp;
-	static class Rocket { 
-		int r, c, w, pre;
-		
-		public Rocket(int r, int c, int w, int pre) {
-			this.r = r;
-			this.c = c;
-			this.w = w;
-			this.pre = pre;
-		}
-	}
+	static int[] dc = {-1, 0, 1};
+
 
 	public static void main(String[] args) throws Exception{
 		/* 셋팅 */
@@ -27,60 +18,56 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 				
 		board = new int[N][M];
-		dp = new int[3][N][M];
-		
+		dp = new int[N][M][3];
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
-				int x = Integer.parseInt(st.nextToken());
-				board[i][j] = x;
-			}
-		}
-		
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<N; j++ ) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 				Arrays.fill(dp[i][j], INF);
 			}
 		}
 		
-		/* 로직 */
-		for (int j = 0; j < M; j++) {
-            dp[0][0][j] = board[0][j];
-            dp[1][0][j] = board[0][j];
-            dp[2][0][j] = board[0][j];
-        }
-		
-		for (int i = 1; i < N; i++) {
-		    for (int j = 0; j < M; j++) {
-		        for (int k = 0; k < 3; k++) { 
-		            
-		            int prev = j - dc[k]; 
-		            
-		            if (check(i - 1, prev)) {
-		                for (int p = 0; p < 3; p++) {
-		                    if (k != p) { 
-		                    	dp[k][i][j] = Math.min(dp[k][i][j], dp[p][i - 1][prev] + board[i][j]);		                    }
-		                }
-		            }
-		        }
-		    }
+		for(int j=0; j<M; j++) {
+			for(int k=0; k<3; k++) {
+				dp[0][j][k] = board[0][j];
+			}
 		}
 		
+		/* 로직 */
+		
+		for(int i=1; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				for(int k=0; k<3; k++) {
+					
+					int prev_c = j - dc[k];
+					
+					if(check(i, prev_c)) {
+						for(int p=0; p<3; p++) {
+							if(k != p) {
+								dp[i][j][k] = Math.min(dp[i][j][k], dp[i-1][prev_c][p] + board[i][j]);								
+							}
+						}
+						
+					}
+				}
+			}
+		}
+
 		
 		/* 출력 */
-		int result = INF;
-        for (int j = 0; j < M; j++) {
-            for (int k = 0; k < 3; k++) {
-                result = Math.min(result, dp[k][N - 1][j]); // 마지막 행(N-1)의 최솟값 찾기
-            }
-        }
-        
-        System.out.print(result);
+		int min = INF;
+		for(int j=0; j<M; j++) {
+			for(int k=0; k<3; k++) {
+				min = Math.min(min, dp[N-1][j][k]);
+			}
+		}
+		System.out.print(min);
 	}
 
-	static boolean check(int nr, int nc) {
-		return (0 <= nr && nr < N) && (0 <= nc && nc < M);
+
+	static boolean check(int nr, int prev_c) {
+		return (0 <= nr && nr < N) && (0 <= prev_c && prev_c < M);
 	}
 
 }
