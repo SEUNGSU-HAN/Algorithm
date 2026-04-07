bf7aedd5aa3983e80959296d81a01d14750fbb10
SEUNGSU HAN <71253519+SEUNGSU-HAN@users.noreply.github.com>
2026-04-07 17:14:25 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -24,13 +24,14 @@ public class Main {
 		dp[0] = 1; //자기 자신일 경우가 dp[0]일 때
 		
 		/* 로직 */
-		Arrays.sort(coin);
 		
 		//동전을 기준으로 탐색
-		for(int c=0; c<N; c++) {
-			//동전을 기준으로 k원을 맞추기 위한 경우를 dp함
-			for(int k=coin[c]; k<=K; k++) {
-				if(k >= c) dp[k] = dp[k] + dp[k-coin[c]];
+		for(int i=0; i<N; i++) {
+			int c = coin[i];
+			
+			// 현재 동전의 가치부터 목표 금액 K까지 누적
+			for(int k=c; k<=K; k++) {
+				dp[k] = dp[k] + dp[k-c];
 			}
 		}
 		
